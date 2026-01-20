61c09a815ecd92e944e0c9ab0b136b02cc77967a
Sofia-kosar <Sofiia.Kosar.RI.2023@lpnu.ua>
2026-01-21 00:50:34 +0200

    fix: temporarily comment out Key Vault secret to resolve RBAC issues

--- infra/appservice.tf
+++ infra/appservice.tf
@@ -39,14 +39,14 @@ resource "azurerm_linux_web_app" "api" {
 
   virtual_network_subnet_id = azurerm_subnet.webapp_integration.id
   app_settings = {
-    WEBSITES_PORT                              = "8080"
-    WEBSITE_CONTENTOVERVNET                    = "1"
-    WEBSITE_PULL_IMAGE_OVER_VNET               = "1"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE        = "false"
-    SQL_SERVER_FQDN                            = azurerm_mssql_server.sql.fully_qualified_domain_name
-    SQL_ADMIN_LOGIN                            = var.sql_admin_login
+    WEBSITES_PORT                       = "8080"
+    WEBSITE_CONTENTOVERVNET             = "1"
+    WEBSITE_PULL_IMAGE_OVER_VNET        = "1"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    SQL_SERVER_FQDN                     = azurerm_mssql_server.sql.fully_qualified_domain_name
+    SQL_ADMIN_LOGIN                     = var.sql_admin_login
     # ТИМЧАСОВО: пароль напряму (Key Vault secret закоментовано через RBAC issues)
-    SQL_ADMIN_PASSWORD                         = var.sql_admin_password
+    SQL_ADMIN_PASSWORD = var.sql_admin_password
     # SQL_ADMIN_PASSWORD                         = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.sql_password.versionless_id})"
     APPLICATIONINSIGHTS_CONNECTION_STRING      = azurerm_application_insights.appi.connection_string
     ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
